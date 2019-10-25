d09f079a4f1fbe982cc8e99c2bf8bc7f71f45de0
Johan Hubau <johan.hubau@gmail.com>
2019-10-25 10:16:32 +0200

    Rework Header template
    Signed-off-by: Johan Hubau <johan.hubau@gmail.com>

--- src/components/templates/header.vue
+++ src/components/templates/header.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
-    <h1>CSC4101</h1>
-    <p>
-      <router-link to="/regions">Go to Region list</router-link>
-      <router-link to="/rooms">Go to Rooms list</router-link>
-    </p>
+    <nav class="px-5 navbar navbar-expand-lg navbar-dark bg-primary">
+      <router-link class="navbar-brand" to="/">RVoy</router-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/regions">Regions</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/rooms">Rooms</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
