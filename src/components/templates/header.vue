3bd04fd771a6c6eb8d13f31d07a52fa440bbac64
Johan Hubau <johan.hubau@gmail.com>
2019-11-22 12:10:58 +0100

    Rework routes for user
    Signed-off-by: Johan Hubau <johan.hubau@gmail.com>

--- src/components/templates/header.vue
+++ src/components/templates/header.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <nav class="px-5 navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav class="px-5 py-3 navbar navbar-expand-lg navbar-dark bg-primary">
       <router-link class="navbar-brand" to="/">RVoy</router-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <router-link class="nav-link" to="/regions">Regions</router-link>
+            <router-link class="nav-link" to="/reserve">Make a reservation</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/rooms">Rooms</router-link>
+            <router-link class="nav-link" to="/login">Connect</router-link>
           </li>
         </ul>
       </div>
