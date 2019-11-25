813c50ed88075f04a83d7f56f208a1d766bc485e
Johan Hubau <johan.hubau@gmail.com>
2019-11-25 16:44:12 +0100

    Add private/public versions
    Signed-off-by: Johan Hubau <johan.hubau@gmail.com>

--- src/components/templates/ProfileBar.vue
+++ src/components/templates/ProfileBar.vue
@@ -1,5 +1,4 @@
 <template>
-  <div class="container">
     <div class="profile-container">
       <div class="picture p-4">
         <img class="profile" src="" alt="Test">
@@ -8,32 +7,37 @@
         <h2 class="pb-3">Lisa Turner</h2>
         <div class="text-right mr-4 p-4">
           <p class="my-1">42 Years Old</p>
-          <p class="my-1">(+33) 6 11 22 33 44</p>
-          <p class="my-1">lisa.turner@exemple.com</p>
-          <p class="my-1">Unknown address</p>
+          <p v-if="isPrivate" class="my-1">(+33) 6 11 22 33 44</p>
+          <p v-if="isPrivate" class="my-1">lisa.turner@exemple.com</p>
+          <p v-if="isPrivate" class="my-1">Unknown address</p>
         </div>
       </div>
       <div class="text-right mr-4 pt-2">
-        <a class="py-1 nav-link" href="">Account settings</a>
-        <a class="py-1 nav-link" href="">Logout</a>
+        <a v-if="!isPrivate" class="py-1 nav-link" href="">See home listings</a>
+        <a v-if="isPrivate" class="py-1 nav-link" href="">Account settings</a>
+        <a v-if="isPrivate" class="py-1 nav-link" href="">Logout</a>
       </div>
     </div>
-  </div>
 </template>
 
 <script>
     export default {
-        name: "ProfileBar"
+        name: "ProfileBar",
+        props: [
+            'isPrivate',
+        ],
     }
 </script>
 
 <style scoped>
   .profile-container {
-    max-width: 33%;
+    background-color: white;
+    z-index: 1;
     width: 400px;
-    min-width: 300px;
     position: fixed;
     right: 0;
+    left: auto;
+    flex-flow: column nowrap;
     height: 100%;
     -webkit-box-shadow: -2px 0px 5px -2px rgba(0, 0, 0, 0.3);
     -moz-box-shadow: -2px 0px 5px -2px rgba(0, 0, 0, 0.3);
