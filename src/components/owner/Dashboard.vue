6b28d2ab504aee1708d8953d973d578d8edc8f78
Johan Hubau <johan.hubau@gmail.com>
2019-11-27 18:47:51 +0100

    Make OwnerDashboard more dynamic
    Signed-off-by: Johan Hubau <johan.hubau@gmail.com>

--- src/components/owner/Dashboard.vue
+++ src/components/owner/Dashboard.vue
@@ -3,14 +3,19 @@
     <profile-template
       :isPrivate="1">
     </profile-template>
-    <div class="modal fade" id="roomDialog" tabindex="-1" role="dialog" aria-hidden="true">
+    <div v-if="rooms" v-for="(listing, index) in rooms" class="modal fade" :id="'roomDialog' + index" tabindex="-1" role="dialog" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
-          <room-template></room-template>
+          <room-template
+            :name="listing['summary']"
+            :description="listing['description']"
+            :superficy="listing['superficy']"
+            :capacity="listing['capacity']"
+            :price="listing['price']"></room-template>
         </div>
       </div>
     </div>
-    <div class="modal fade" id="reservationDialog" tabindex="-1" role="dialog" aria-hidden="true">
+    <div v-if="reservations" v-for="reservation in reservations" class="modal fade" :id="'reservationDialog' + index" tabindex="-1" role="dialog" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
           <reservation-template></reservation-template>
@@ -18,11 +23,17 @@
       </div>
     </div>
     <div class="container-fluid">
+      <div v-if="reservationLoading || roomLoading" class="w-100">
+        <div class="progress">
+          <div class="progress-bar progress-bar-striped progress-bar-animated bg-primary" role="progressbar"
+               aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
+        </div>
+      </div>
       <div class="row">
         <div class="col px-0">
           <button type="button" class="btn btn-secondary btn-block px-0" @click="ToListings()">Your Listings</button>
         </div>
-        <div class="col px-0">
+        <div v-if="isPrivate" class="col px-0">
           <button type="button" class="btn btn-secondary btn-block px-0" @click="ToReservations()">Reservations</button>
         </div>
       </div>
@@ -30,25 +41,25 @@
         <transition name="fade" mode="out-in">
           <div v-if="section === 'listing'" key="1">
             <h2>Listings</h2>
-            <div v-for="listing in listings" class="col-lg px-1 py-2">
-              <div class="reservation-container p-3 row" data-toggle="modal" data-target="#roomDialog">
+            <div v-if="rooms" v-for="(listing, index) in rooms" class="col-lg px-1 py-2">
+              <div class="reservation-container p-3 row" data-toggle="modal" :data-target="'#roomDialog' + index">
                 <div class="p-3">
                   <img src="" alt="Room Image">
                 </div>
                 <div class="p-3">
-                  <h5>{{ listing.name }}</h5>
-                  <p class="my-1">{{listing.description}} </p>
+                  <h5>{{ listing['summary'] }}</h5>
+                  <p class="my-1">{{ listing['description'] }} </p>
                 </div>
               </div>
             </div>
           </div>
           <div v-if="section === 'reservation'" key="2">
             <h2>Reservations</h2>
-            <div v-for="reservation in reservations" class="col-lg px-1 py-2">
-              <div class="reservation-container p-3" data-toggle="modal" data-target="#reservationDialog">
-                <h5>From {{ reservation.from }} To {{ reservation.until }}</h5>
-                <p class="my-1">Client: {{reservation.client}} </p>
-                <p class="my-1">Listing: {{reservation.room}}</p>
+            <div v-if="reservations" v-for="reservation in reservations" class="col-lg px-1 py-2">
+              <div class="reservation-container p-3" data-toggle="modal" :data-target="'#reservationDialog' + index">
+                <h5>From {{ reservation['from'] }} To {{ reservation['until'] }}</h5>
+                <p class="my-1">Client: {{ reservation['client'] }} </p>
+                <p class="my-1">Listing: {{reservation ['room'] }}</p>
               </div>
             </div>
           </div>
@@ -67,21 +78,41 @@
         data() {
             return {
                 section: "listing",
-                reservations: [
-                    {from: '12/10/16', until: '14/10/16', client: 'John Smith', room: 'Small home in Edimburgh'},
-                    {from: '11/11/16', until: '14/11/16', client: 'Johnny Smith', room: 'Small home in Edimburgh'},
-                    {from: '12/10/16', until: '14/10/16', client: 'John Smith', room: 'Barcelona'},
-                    {from: '12/10/16', until: '14/10/16', client: 'John Smith', room: 'Small home in Edithtown'},
-                ],
-                listings: [
-                    {name: 'Small home in Edimburgh', description: "It's a lovely home motherfucker"},
-                    {name: 'Barcelona', description: "Artists are welcome"},
-                    {name: 'Shithole in Paris', description: "I'm not kidding it's really a shithole"},
-                ],
+                reservationLoading: false,
+                roomLoading: false,
+                isPrivate: true,
             }
         },
 
+        computed: mapGetters({
+            reservationDeletedItem: 'reservation/del/deleted',
+            reservationError: 'reservation/list/error',
+            reservations: 'reservation/list/items',
+            reservationsIsLoading: 'reservation/list/isLoading',
+            roomDeletedItem: 'room/del/deleted',
+            roomError: 'room/list/error',
+            rooms: 'room/list/items',
+            roomIsLoading: 'room/list/isLoading',
+        }),
+
+        created() {
+            this.roomLoading = true;
+            this.$store.dispatch('room/list/default', {owners: "/api/owners/" + this.$route.params.id}).then(() => {});
+            this.roomLoading = false;
+            if (this.isPrivate) {
+                this.reservationLoading = true;
+                this.$store.dispatch('reservation/list/default', {owners: "/api/owners/" + this.$route.params.id}).then(() => {});
+                this.reservationLoading = false;
+            }
+
+        },
+
         methods: {
+            ...mapActions({
+                del: 'owner/del/del',
+                reset: 'owner/show/reset',
+                retrieve: 'owner/show/retrieve'
+            }),
 
             ToListings() {
                 this.section = "listing"
