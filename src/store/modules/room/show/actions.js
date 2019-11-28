d48f67caf517da8f3fc0d9248eecbc6a8723d8f4
Johan Hubau <johan.hubau@gmail.com>
2019-11-28 18:43:54 +0100

    Make Promise for actions
    Signed-off-by: Johan Hubau <johan.hubau@gmail.com>

--- src/store/modules/room/show/actions.js
+++ src/store/modules/room/show/actions.js
@@ -4,16 +4,20 @@ import * as types from './mutation_types'
 export const retrieve = ({ commit }, id) => {
   commit(types.ROOM_SHOW_TOGGLE_LOADING)
 
-  return fetch(id)
+  return new Promise((resolve, reject) => {
+    return fetch(id)
     .then(response => response.json())
     .then((data) => {
       commit(types.ROOM_SHOW_TOGGLE_LOADING)
       commit(types.ROOM_SHOW_SET_RETRIEVED, data)
+      resolve();
     })
     .catch((e) => {
       commit(types.ROOM_SHOW_TOGGLE_LOADING)
       commit(types.ROOM_SHOW_SET_ERROR, e.message)
+      reject();
     })
+  });
 }
 
 export const reset = ({ commit }) => {
